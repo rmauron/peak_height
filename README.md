c74c19ed076cc1de874f576907ff2604b3a1cc9d
Raphaël Mauron <raphael.mauron@gmail.com>
2023-04-16 11:37:23 +0200

    Finalized peak_height project

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # peak_height
+
+As part of a fellow student's master's work, the height of peak series had to be measured. I ensured to facilitate the task with this script that automatically indicates these distances without the need to manually manipulate the data beforehand.
+
+## Prerequisities
+All you need is to have a directory with the script "plot_peak_height.py", specify your path to the directory in line 9 of the script.
+
+Organize your project directory in the following manner:
+
+peak_height_project
+├── README.md
+├── data
+│   ├── Offline_test1_SN_7_1.txt
+│   ├── Offline_test1_SN_7_2.txt
+│   ├── Offline_test1_SN_7_3.txt
+│   └── Offline_test1_SN_7_4.txt
+└── plot_peak_height.py
+
+with all the data files in the data repository and the script.
+
+Once you run the script, a folder "output" is automatically generated and stores every plot with the same name as the data inputs:
+
+peak_height_project
+├── README.md
+├── data
+│   ├── Offline_test1_SN_7_1.txt
+│   ├── Offline_test1_SN_7_2.txt
+│   ├── Offline_test1_SN_7_3.txt
+│   └── Offline_test1_SN_7_4.txt
+├── output
+│   ├── Offline_test1_SN_7_1.png
+│   ├── Offline_test1_SN_7_2.png
+│   ├── Offline_test1_SN_7_3.png
+│   └── Offline_test1_SN_7_4.png
+└── plot_peak_height.py
+
+At this point, the work is done.
--- plot_peak_height.py
+++ plot_peak_height.py
@@ -1,10 +1,11 @@
 # libraries
-import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import argrelextrema
+import numpy as np
 import os
+import re
+from scipy.signal import argrelextrema
 
-# Set the directory where you have the data file
+# Set the directory to you project directory
 os.chdir('/Users/raphaelmauron/peak_height')
 
 # create output directory if not already existing
@@ -13,61 +14,71 @@ if not os.path.exists('output'):
 else:
     print('Path already exists.')
 
+# function to read file, plot and save the plots
+def read_plot_save(my_file):
 
-# iterate through all the files found in '/data' directory
+    # open and read the file
+    with open(my_file, 'r') as file:
+        # read the lines in the file, skipping the 1st row
+        lines = file.readlines()[1:]
+
+    # create empty lists 
+    x_values = []
+    y_values = []
+
+    # iterate over the lines
+    # replace ',' by '.' & convert to float
+    # append x and y values
+    for line in lines:
+        x, y, _ = line.split(maxsplit=2)
+        x = float(x.replace(',', '.'))
+        y = float(y.replace(',', '.'))
+        x_values.append(x)
+        y_values.append(y)
+        
+    # lists to plots
+    x_values = np.array(x_values)
+    y_values = np.array(y_values)
+
+    # find local maxima above x=-500
+    maxima_idx = argrelextrema(y_values, np.greater)[0]
+    maxima_above_y = []
+    for i in maxima_idx:
+        if y_values[i] >= -500:
+            maxima_above_y.append(i)
+
+    # plot the data and distances
+    fig, ax = plt.subplots()
+    ax.plot(x_values, y_values)
+
+    # mark the maxima with red dots
+    for i in range(len(maxima_above_y)):
+        if i > 0 and y_values[maxima_above_y[i]] - y_values[maxima_above_y[i-1]] >= 90:
+            x_r = x_values[maxima_above_y[i-1]]
+            y_r = y_values[maxima_above_y[i-1]]
+            x_g = x_values[maxima_above_y[i]]
+            y_g = y_values[maxima_above_y[i]]
+            ax.plot(x_r, y_r, 'ro', markersize=5)
+            ax.plot(x_g, y_g, 'go', markersize=5)
+            # draw a vertical line between the red and green dots
+            ax.text(x_g, y_g-70, f"{y_g-30:.2f}", ha='center', va='top', fontsize=8)
+
+    # plot the data using matplotlib
+    my_dest = str(my_file[re.search('data/', my_file).end():]) + ".png" #retrieve file name for saving and plot title
+    my_dest = my_dest.replace('.txt', '')
+    my_title = my_dest.replace('.png', '')
+    
+    #plot options
+    plt.xlabel('Time [s]')
+    plt.ylabel('LSPR Response [pm]')
+    plt.title(my_title)
+    plt.savefig('./output/' + my_dest, dpi=300)
+    plt.clf()
+
+# iterate through all the files found in './data' directory
 for filename in os.listdir('data'):
     f = os.path.join('data', filename)
     # checking if it is a file
     if os.path.isfile(f):
-        print(f)
-
-# open and read the file
-my_file = './data/Offline_test1_SN_7_1.txt'
-
-
-with open(my_file, 'r') as file:
-
-    # read the lines in the file, skipping the 1st row
-    lines = file.readlines()[1:]
-
-# create empty lists 
-x_values = []
-y_values = []
-
-# iterate over the lines
-# replace , by . & convert to float
-# append x and y values
-for line in lines:
-    x, y, _ = line.split(maxsplit=2)
-
-    x = float(x.replace(',', '.'))
-    y = float(y.replace(',', '.'))
-
-    x_values.append(x)
-    y_values.append(y)
-
-x_values = np.array(x_values)
-y_values = np.array(y_values)
-
-
-# find local maxima and minima
-maxima_idx = argrelextrema(y_values, np.greater)[0]
-minima_idx = argrelextrema(y_values, np.less)[0]
-
-# compute distances between maxima and minima
-distances = np.abs(y_values[maxima_idx] - y_values[minima_idx])
-
-# plot the data and distances
-fig, ax = plt.subplots()
-ax.plot(x_values, y_values)
-for i in range(len(distances)):
-    ax.text((x_values[maxima_idx[i]] + x_values[minima_idx[i]]) / 2, 
-            (y_values[maxima_idx[i]] + y_values[minima_idx[i]]) / 2, 
-            '{:.2f}'.format(distances[i]), 
-            ha='center', va='center')
-
-plt.xlabel('Time [s]')
-plt.ylabel('LSPR Response [pm]')
-
-
-plt.show()
+        read_plot_save(f) #execute the function
+print("The task is done. Find your plots and peak heights in the created output folder!")
